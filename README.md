6615984131d46e0c4f6c8d92021b562e28f6b9f0
nlovas <nlovas@ualberta.ca>
2017-02-05 20:43:05 -0700

    passes freetests

--- README.md
+++ README.md
@@ -13,10 +13,14 @@ I referenced Ryan Satyabrata's repo to figure out how to begin, how to check if 
 (https://github.com/kobitoko/CMPUT404-assignment-web-client)
 Profile: (https://github.com/kobitoko)
 
+I consulted and pair programmed with Jillian Lovas, CCID jlovas
+
 To learn what argv[0] meant in python:
 https://docs.python.org/2/library/sys.html
 and
-http://stackoverflow.com/questions/4117530/sys-argv1-meaning-in-script
+(http://stackoverflow.com/a/4118133)
+Answered by Jason R. Coombs on Stack Overflow
+(http://stackoverflow.com/users/70170/jason-r-coombs)
 
 Understanding why python has assignments inside function parameters:
 http://pythoncentral.io/fun-with-python-function-parameters/
@@ -28,8 +32,18 @@ How to begin a GET connection as a client from the socket library:
 https://docs.python.org/2/library/socket.html#socket.socket.connect
 
 how to start a python for loop at a certain spot
-		#(http://stackoverflow.com/a/14053750)
-		#answered by Inbar Rose on Stack Overflow (http://stackoverflow.com/users/1561176/inbar-rose)
+(http://stackoverflow.com/a/14053750)
+answered by Inbar Rose on Stack Overflow (http://stackoverflow.com/users/1561176/inbar-rose)
+
+How to disable unit tests:
+(http://stackoverflow.com/a/16138561)
+Answered by yoni on Stack Overflow (http://stackoverflow.com/users/183983/yoni)
+
+Remove the last character from a string:
+(http://stackoverflow.com/a/15478161) Answered by Cyrille on Stack Overflow
+(http://stackoverflow.com/users/2110805/cyrille)
+
+information on application/x-www-form-urlencoded (https://www.w3.org/TR/html401/interact/forms.html)
 
 Contributors / Licensing
 ========================
--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2017 Abram Hindle, https://github.com/tywtyw2002, https://github.com/treedust, and Nicole Lovas
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,125 +25,143 @@ import re
 import urllib
 
 def help():
-	print "httpclient.py [GET/POST] [URL]\n"
+    print "httpclient.py [GET/POST] [URL]\n"
 
 class HTTPResponse(object):
-	def __init__(self, code=200, body=""):
-		self.code = code
-		self.body = body
+    def __init__(self, code=200, body=""):
+        self.code = code
+        self.body = body
 
 class HTTPClient(object):
+    #def get_host_port(self,url):
 
-	def get_host_port(self,url):
-		#can sometimes be given 127.0.0.1:8080 ! so break it down and check
-		#referenced Ryan Satyabrata's repo to figure out how to check(https://github.com/kobitoko/CMPUT404-assignment-web-client)
-		ipPort = url.split(':')
-		print ipPort[0:]
-		if(len(ipPort) > 1):
-			return ipPort[1]
-		else:
-			return 80	
-			
+    def connect(self, host, port):
+	#from the example in https://docs.python.org/2/library/socket.html#socket.socket.connect
 
-	def getRoot(self, url):
-	#check if its an ip or a regular url and return the root, but if it's an ip it will have the port at the end
-	#referenced Ryan Satyabrata's repo to figure out how to check(https://github.com/kobitoko/CMPUT404-assignment-web-client)
-		pieces = url.split('/')
-		#print pieces[0:]
-		if(pieces[0] == 'http:' or pieces[0] == 'https:'):
-			return pieces[2] 
-		else:
-			return pieces[0]
+        cskt = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	port = int(port)
+	cskt.connect(( host,port ))
+        return cskt
 
-	def checkIfIP(self, mayberoot):
-		root = mayberoot.split(':')
-		if(len(root)>1):
-			return root[0]
-		else:
-			return mayberoot
-		
-	def afterRoot(self, url, root):
-	#get the pages after the root url, if there are any
-	#in this case the port # is included for ips, we're looking for whats after it
-		pages = url.split('/')
-		i = 0
-		print "url: " + url + " root: " + root
-		print pages[0:]
-		#get the parts only after the one that == the root
-		for index in range(len(pages)):
-			if(pages[index] == root):
-				i = index
-				break
-		
-		#how to start a python for loop at a certain spot
-		#(http://stackoverflow.com/a/14053750)
-		#answered by Inbar Rose on Stack Overflow (http://stackoverflow.com/users/1561176/inbar-rose)
-		after = ""
-		for index in range(i, len(pages)):
-			print index
-			if(pages[index] != "" and pages[index]!=root):
-				after = after + "/" + pages[index]
-		if (after == ""):
-			after = '/'			
-		return after
+    def get_code(self, data):
+	parts = data.split(" ")
+	kode = parts[1]
+	return kode
 
+    def get_headers(self,data):
+	headers = data.split('\r\n\r\n')
+	return headers[0]
 
-	def connect(self, host, port):
-		#from the example in https://docs.python.org/2/library/socket.html#socket.socket.connect
-		#print "host:" + host + " port: " + str(port)
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((host, port))
-		return s
-
-	def get_code(self, data):
-		return None
-
-	def get_headers(self,data):
-		return None
-
-	def get_body(self, data):
-		return None
+    def get_body(self, data):
+	parts = data.split('\r\n\r\n')
+	bodi = parts[1]        
+	return bodi
 
     # read everything from the socket
-	def recvall(self, sock):
-		buffer = bytearray()
-		done = False
-		while not done:
-			part = sock.recv(1024)
-			if (part):
-				buffer.extend(part)
-			else:
-				done = not part
-			return str(buffer)
+    def recvall(self, sock):
+        buffer = bytearray()
+        done = False
+        while not done:
+            part = sock.recv(1024)
+            if (part):
+                buffer.extend(part)
+            else:
+                done = not part
+        return str(buffer)
 
-	def GET(self, url, args=None):
-		
-		rootURL = self.getRoot(url)
-		pgs = self.afterRoot(url, rootURL)
-		hostPort = self.get_host_port(rootURL)		
-		rootURL = self.checkIfIP(rootURL) #get rid of a trailing port number if it's an ip		
-		s = self.connect(rootURL,int(hostPort))
-		request = "GET" + pgs + "HTTP/1.1\r\n\r\n"
-		s.sendall(request)
+    def GET(self, url, args=None):
+	#I had to redo this sooo many times and just restarted so. There's no function calls for this.
+	#get the host
+	pieces = url.split('http://')
+	hostwpnpgs = pieces[1]
+	splitslash = hostwpnpgs.split('/')
+	hostwp = splitslash[0]
+	#but if it has a port in it, get the port and shorten the url
+	port = 80
 
-		response = self.recvall(s)
+	if(':' in hostwp):
+#referenced Ryan Satyabrata's repo to figure out how to check if there's a port in the url(https://github.com/kobitoko/CMPUT404-assignment-web-client)
+		spltport = hostwp.split(':')		
+		port = spltport[1]
+		root = spltport[0]
+	else:
+		root = hostwp #there's no port in the url
 
-		code = 500
-		body = ""
-		return HTTPResponse(code, body)
+	port = int(port)
+	
+	#get the stuff after the hostname, if there is anything
+	pages = "/"
+#how to start a python for loop at a certain spot
+		#(http://stackoverflow.com/a/14053750)
+#answered by Inbar Rose on Stack Overflow (http://stackoverflow.com/users/1561176/inbar-rose)
+	for item in range(1, len(splitslash)):
+		pages = pages + splitslash[item] + '/'
+	pages = pages[:-1] # How to remove the last character from a string (http://stackoverflow.com/a/15478161) on Stack Overflow (http://stackoverflow.com/users/2110805/cyrille)
 
-	def POST(self, url, args=None):
-		code = 500
-		body = "" #urllib.encode(dictionary) #TODO: put a dcitonary variable here...
-		return HTTPResponse(code, body)
+	header = "GET " + pages + " HTTP/1.1\r\nHost: " + root + "\r\n" + "Accept: */*\r\n\r\n"
+	
+	clnt = self.connect(root, port)
+	clnt.sendall(header)
+	response = self.recvall(clnt)
 
-	def command(self, url, command="GET", args=None):
-		if (command == "POST"):
-			print "post!"
-			return self.POST( url, args )
-		else:
-			print "get!"
-			return self.GET( url, args )
+	print response
+	
+	code = self.get_code(response)
+	body = self.get_body(response)
+	
+	code = int(code)
+        return HTTPResponse(code, body)
+
+    def POST(self, url, args=None):
+
+	pieces = url.split('http://')
+	hostwpnpgs = pieces[1]
+	splitslash = hostwpnpgs.split('/')
+	hostwp = splitslash[0]
+	#but if it has a port in it, get the port and shorten the url
+	port = 80
+
+	if(':' in hostwp):
+		spltport = hostwp.split(':')		
+		port = spltport[1]
+		root = spltport[0]
+	else:
+		root = hostwp #there's no port in the url
+
+	port = int(port)
+#get the stuff after the hostname, if there is anything
+	pages = "/"
+	for item in range(1, len(splitslash)):
+		pages = pages + splitslash[item] + '/'
+	pages = pages[:-1] # How to remove the last character from a string (http://stackoverflow.com/a/15478161) on Stack Overflow (http://stackoverflow.com/users/2110805/cyrille)
+	clnt = self.connect(root, port)
+	poster = "POST " + pages + " HTTP/1.1\r\nHost: " + root + "\r\n"
+
+	#referenced Ryan Satyabrata's repo to figure out how to handle args(https://github.com/kobitoko/CMPUT404-assignment-web-client)
+	encargs = ""
+	if(args != None):
+		encargs = urllib.urlencode(args)
+	#information on application/x-www-form-urlencoded (https://www.w3.org/TR/html401/interact/forms.html)
+	ctype = "content-type: application/x-www-form-urlencoded\r\n" 
+	clen = "content-length: " + str(len(encargs)) + "\r\n"
+	hnbody = poster + clen + ctype + "\r\n" + encargs + "\r\n\r\n"
+	
+	clnt.sendall(hnbody)
+	response = self.recvall(clnt)
+	header = self.get_headers(response)
+	
+	print response
+
+	code = self.get_code(response)
+	body = self.get_body(response)
+	code = int(code)
+        return HTTPResponse(code, body)
+
+    def command(self, url, command="GET", args=None):
+        if (command == "POST"):
+            return self.POST( url, args )
+        else:
+            return self.GET( url, args )
     
 if __name__ == "__main__":
     client = HTTPClient()
